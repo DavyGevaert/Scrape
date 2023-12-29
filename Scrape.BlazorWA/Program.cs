de80f7c0f7f0ee4206521bd236cafedce17a2393
DavyGevaert <gevaert.davy@gmail.com>
2023-12-29 12:06:24 +0100

    added bootstrap to blazor project, Chrome does not work, have to rely on FireFox or Edge

--- Scrape.BlazorWA/Program.cs
+++ Scrape.BlazorWA/Program.cs
@@ -11,4 +11,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddTransient<Data>();
 
+builder.Services.AddBlazorBootstrap();
+
 await builder.Build().RunAsync();
